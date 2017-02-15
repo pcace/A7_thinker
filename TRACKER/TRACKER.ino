032655cfc15e0d21b3f6a950088200c1327cf257
pcace <contact@johannes-froelich.de>
2017-02-15 19:26:39 +0100

    rnd

--- TRACKER/TRACKER.ino
+++ TRACKER/TRACKER.ino
@@ -1,20 +1,24 @@
 int PWR_PIN = 2;
-
+String content = "";
+String character;
 void setup() {
 
   // START A7 with 2 seconds 3,3V on PWR PIN
+
   pinMode(PWR_PIN, OUTPUT);
   digitalWrite(PWR_PIN, HIGH);
-  delay(2000);
+  delay(1000);
   digitalWrite(PWR_PIN, LOW);
 
+  // SETUP SERIAL INTERFACE
 
+  Serial.begin(9600);
+  Serial1.begin(112500);
+  Serial.println("INIT");
 }
+
+
 
 void loop() {
-
-
-
+ 
 }
-
-
